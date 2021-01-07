0af2f95e6026b4709522c8ca08c2cfa88338931f
Sarath <sarath@qdmplatforms.com>
2021-01-08 01:12:25 +0530

    list screen completed

--- src/router/index.js
+++ src/router/index.js
@@ -8,8 +8,8 @@ import routes from "./routes";
 import PrivateRoute from "./private_router";
 
 import {
-    Login,
-    SignUp,
+    // Login,
+    // SignUp,
     Home,
     Profile
 } from './../screens'
@@ -17,9 +17,9 @@ import {
 const RouterApp = (props) => {
     return <Router>
         <Switch>
-            <Route exact path="/" component={Login}/>
-            <Route path={routes.login} component={Login}/>
-            <Route path={routes.signup} component={SignUp}/>
+            <Route exact path="/" component={Home}/>
+            {/* <Route path={routes.login} component={Login}/> */}
+            {/* <Route path={routes.signup} component={SignUp}/> */}
             <PrivateRoute path={routes.home} component={Home} />
             <PrivateRoute path={routes.profile} component={Profile} />
         </Switch>
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,15 +1,85 @@
 import React from 'react';
-import { Typography, Button } from '@material-ui/core';
 import { localStorageKeys } from '../../utils';
-import { withRouter } from 'react-router-dom';
 import routes from '../../router/routes';
+import AppBar from  "../../components/topBar";
+import { withStyles, Container} from '@material-ui/core';
+import List from "./formList";
+import Search from "./search";
+import FormModal from "./addFormModal";
+
+const data = [
+    {
+        form_name: "Full Body Assessment",
+        created_by: "Ajay",
+        created_at: new Date(),
+        category: "Assessment"
+   },
+   {
+        form_name: "Progress note",
+        created_by: "Ajay",
+        created_at: new Date(),
+        category: "Report"
+   },
+   {
+    form_name: "Progress note",
+    created_by: "Ajay",
+    created_at: new Date(),
+    category: "Report"
+  },
+  {
+    form_name: "Progress note",
+    created_by: "Ajay",
+    created_at: new Date(),
+    category: "Report"
+  },
+  {
+    form_name: "Progress note",
+    created_by: "Ajay",
+    created_at: new Date(),
+    category: "Report"
+  },
+  {
+    form_name: "Progress note",
+    created_by: "Ajay",
+    created_at: new Date(),
+    category: "Report"
+ },
+ {
+    form_name: "Progress note",
+    created_by: "Ajay",
+    created_at: new Date(),
+    category: "Report"
+ },
+]
+
+const styles = theme => ({
+    root: {
+        backgroundColor: "#ebebeb",
+        flex: 1
+    },
+    mainArea: {
+        backgroundColor: "#ebebeb",
+        height: "calc( 100vh - 49px)",
+        overflow: "auto"
+    },
+    listArea:{
+        paddingTop: "30px"
+    },
+    search: {
+        position: "sticky",
+        zIndex: 1,
+        top: "-1px",
+        background: "#ebebeb",
+    }
+  });
 
 class Home extends React.Component{
     constructor(props){
         super(props);
         this.state = {
             message: 'You are in Home page.',
-            loggingOut : false
+            loggingOut : false,
+            formModal: false,
         }
     }
 
@@ -24,33 +94,39 @@ class Home extends React.Component{
         })
     }
 
-    componentDidMount(){
-        if(!localStorage.getItem(localStorageKeys.auth_token)){
-            this.props.history.push(routes.login);
-        }
+    // componentDidMount(){
+    //     // if(!localStorage.getItem(localStorageKeys.auth_token)){
+    //     //     this.props.history.push(routes.login);
+    //     // }
+    // }
+
+    addNewFormModal = () => {
+        this.setState({...this.state, formModal: true})
     }
-    
+
+    modalClose = () => {
+        this.setState({...this.state, formModal: false})
+    };
+
     render(){
-
-        const {
-            message,
-            loggingOut
-        } = this.state;
-
-        return <div>
-            <Typography>
-                {message}
-            </Typography>
-            <Button
-                color="secondary"
-                variant="outlined"
-                disabled={loggingOut}
-                onClick={this.logOut}
-            >
-                {`${loggingOut ? 'Logging Out...' : 'Log Out'}`}
-            </Button>
+       const { classes } = this.props;
+        return <div className={classes.root}>
+            <AppBar  title={"Form Configurator"} buttonName={"+ add New"} onClickButton={()=> this.addNewFormModal()}/>
+            <div className={classes.mainArea}>
+                <Container maxWidth={"xl"} className={classes.listArea}>
+                    <div className={classes.search}>
+                     <Search />
+                    </div>
+                     {data.map((list, index)=>{
+                         return(
+                             <List key={index} {...list} />
+                         )
+                     })}
+                </Container>
+            </div>
+            <FormModal modalClose={this.modalClose} open={this.state.formModal}/>
         </div>
     }
 }
 
-export default withRouter(Home);
+export default withStyles(styles)(Home);
